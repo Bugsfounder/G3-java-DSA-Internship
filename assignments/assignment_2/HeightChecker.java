96ce89a89a71a9baf91b6264601754be3cbc4909
bugsfounder <bugsfounder2021@gmail.com>
2025-08-03 23:34:58 +0530

    counting sort

---
+++ assignments/assignment_2/HeightChecker.java
@@ -0,0 +1,51 @@
+package assignments.assignment_2;
+
+import java.util.Arrays;
+
+public class HeightChecker {
+
+    static void countSort(int[] unsortedArray, int[] sortedArray){
+        /*
+        1.create array of size 101,
+         because constraint give 1 to 100, but index is starting from 0,
+         we have to add one more element, if we create till 100, we got element till 0 to 99
+        */
+        int[] indexArray = new int[101];
+
+        /*
+         2. iterate over unsortedArray and increase frequency of all elements at elements index in indexArray.
+        */
+
+        for (int item: unsortedArray){
+            indexArray[item]++;
+        }
+
+        /*
+            3. iterate over indexArray, and insert elements using it in sortedArray.
+        */
+        int j = 0;
+        for (int i = 0; i < indexArray.length; i++) {
+            if (indexArray[i]!=0){
+                while (indexArray[i] > 0){
+                    sortedArray[j] = i;
+                    j++;
+                    indexArray[i]--;
+                }
+            }
+            if (j > sortedArray.length){
+                break;
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,4,2,5,3,1,2};
+        int[] sortedArr = new int[arr.length];
+
+        countSort(arr, sortedArr);
+
+        System.out.println(Arrays.toString(sortedArr));
+    }
+
+}
