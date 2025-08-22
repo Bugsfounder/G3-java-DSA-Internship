b28bfd13ebb73533ea6cd90fcc31ed9a64a55387
bugsfounder <bugsfounder2021@gmail.com>
2025-08-22 10:40:28 +0530

    added, add at beginning, and count the no. of node in linked list

--- task_tracking/aug_21_25/readme.md
+++ task_tracking/aug_21_25/readme.md
@@ -52,4 +52,52 @@ public class MyLinkedList {
 ```
 ### TODO
 1. Modify this code to insert at the beginning.
+```java
+// add at beginning
+public void addAtBeginning(int data){
+    Node newNode = new Node(data);
+    if (head==null){
+        head = newNode;
+        return;
+    }
+    newNode.next = head;
+    head = newNode;
+    System.out.println(head.data); // head is now pointing to newNode
+}
+```
 2. Count the number of nodes in the LinkedList
+```java
+// count number of nodes
+public int countNoOfNodes(){
+    Node start = head;
+    int count = 0;
+    while (start!=null){
+        count++;
+        start = start.next;
+    }
+    return count;
+} 
+```
+main
+```java
+public static void main(String[] args) {
+    MyLinkedList list = new MyLinkedList();
+    list.insert(10);
+    list.insert(10);
+    list.insert(10);
+    list.display();
+
+    list.addAtBeginning(20);
+    list.display();
+
+    System.out.println(list.countNoOfNodes());
+
+}
+```
+output
+```java 
+10->10->10->null
+20
+20->10->10->10->null
+4
+```
--- task_tracking/aug_21_25/src/MyLinkedList.java
+++ task_tracking/aug_21_25/src/MyLinkedList.java
@@ -1,11 +1,3 @@
-class Node{
-    int data;
-    Node next;
-    Node(int data){
-        this.data = data;
-        this.next = null;
-    }
-}
 public class MyLinkedList {
     // insert
     Node head;
@@ -30,11 +22,41 @@ public class MyLinkedList {
         }
         System.out.println("null");
     }
+
+    // add at beginning
+    public void addAtBeginning(int data){
+        Node newNode = new Node(data);
+        if (head==null){
+            head = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+        System.out.println(head.data); // head is now pointing to newNode
+    }
+
+    // count number of nodes
+    public int countNoOfNodes(){
+        Node start = head;
+        int count = 0;
+        while (start!=null){
+            count++;
+            start = start.next;
+        }
+
+        return count;
+    }
     public static void main(String[] args) {
         MyLinkedList list = new MyLinkedList();
         list.insert(10);
         list.insert(10);
         list.insert(10);
         list.display();
+
+        list.addAtBeginning(20);
+        list.display();
+
+        System.out.println(list.countNoOfNodes());
+
     }
 }
